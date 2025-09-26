ba5c83ebaaca0f85694ca1effc1968a5a5ea7e2e
MicDeVilla <59424577+MicDeVilla@users.noreply.github.com>
2025-09-26 18:11:08 +0800

    Build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/matt1484/pokemon-unite-designer/master/docs' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/MicDeVilla/pokemon-unite-designer/tree/yarn-copy/docs' : '/',
   devServer: {
     host: '127.0.0.1',
   }
