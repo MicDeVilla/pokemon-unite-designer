2dbbbede08de2c7c0027285da040847b9755142e
MicDeVilla <59424577+MicDeVilla@users.noreply.github.com>
2025-09-27 11:11:12 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/MicDeVilla/pokemon-unite-designer/tree/master/docs' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/MicDeVilla/pokemon-unite-designer/master/docs' : '/',
   devServer: {
     host: '127.0.0.1',
   }
